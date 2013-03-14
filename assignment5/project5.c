7441530bd3afa89429a71246a60ecbb88d1e387d
Frank Nielson <f.nielson@gmail.com>
2013-03-14 16:00:27 -0600

    Beginnings of assignment 5

---
+++ assignment5/project5.c
@@ -0,0 +1,27 @@
+/*
+* Authors:
+* Frank Nielson
+* Caleb Kruger
+
+* Class: CS 3060
+* Project: Assignment 5
+
+* This code has be written by the authors
+* listed above.
+*/
+
+#include <stdio.h>	// stdin, etc
+#include <stdlib.h>	// exit()
+#include <string.h>	// strlen()
+#include <signal.h>	// sigaction and sigset_t
+#include <unistd.h> // write()
+
+int main()
+{
+	// Check if we are a child process
+
+	// If so, set up signal handlers and goto function
+
+	// Else, we are parent; set up different handlers and goto function
+	return 0;
+}
