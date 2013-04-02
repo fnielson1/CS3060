8c123a8ef5b4417ac4131d1fbbcb588673dd8a66
Frank Nielson <f.nielson@gmail.com>
2013-04-02 17:10:24 -0600

    Created the Print function and the First Come First Serve function

--- assignment6/project6.c
+++ assignment6/project6.c
@@ -15,15 +15,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_INPUT_LEN (100)
+#define MAX_INPUT_LEN (200)
 #define BASE_TEN (10)
+#define PRINT_HEADER (1)
+#define PRINT_NORMAL (0)
+#define PRINT_FOOTER (2)
 
 
+void Print(int, int, int, int);
 void InputToArray(char*,int*, int*, int*);
+void FirstCome(const int*, const int*, const int*, const int);
 int StrToInt(char*);
 
+
 // GLOBAL
-// Keep track of the indexes on the arrays
+// Keep track of the indexes on the 
+// arrays (one job added after another.
 int _pidIndex = 0;
 
 
@@ -32,15 +39,15 @@ int main()
 	char input[MAX_INPUT_LEN];
 	int arrPid[MAX_INPUT_LEN];
 	int arrArrival[MAX_INPUT_LEN];
-	int arrTime[MAX_INPUT_LEN];
+	int arrService[MAX_INPUT_LEN];
 
 	// Read data from stdin
 	while(fgets(input, MAX_INPUT_LEN, stdin) != 0)
 	{
-		InputToArray(input, arrPid, arrArrival, arrTime);
+		InputToArray(input, arrPid, arrArrival, arrService);
 	}
 	// Call function to print the 3 arrays
-
+	FirstCome(arrPid, arrArrival, arrService, _pidIndex);
 	return 0;
 }
 
@@ -54,9 +61,9 @@ int main()
 * const char* input: The input string to parse.
 * int* arrPid: The array that will have the PIDs.
 * int* arrArrival: The time the job arrived (ms)
-* int* arrTime: The time for the job to complete (ms)
+* int* arrService: The time for the job to complete (ms)
 */
-void InputToArray(char* input, int* arrPid, int* arrArrival, int* arrTime)
+void InputToArray(char* input, int* arrPid, int* arrArrival, int* arrService)
 {
 	char *splitStr;
 	char *delim = "\t "; // Split on tabs and spaces
@@ -79,7 +86,7 @@ void InputToArray(char* input, int* arrPid, int* arrArrival, int* arrTime)
 			firstLoop = 1;
 		}	
 		else
-			arrTime[_pidIndex] = number;
+			arrService[_pidIndex] = number;
 	}
 	++_pidIndex; // Increment our index
 }
@@ -109,5 +116,105 @@ int StrToInt(char *buffer)
 		exit(1);
 	}
 	return number;
+}
+
+/*
+** FirstCome
+
+* Takes 3 arrays that simulate jobs entering the
+	waiting queue. It then 'runs' each of the jobs,
+	keeping track of various calculations.
+
+* const int* arrPid: The PIDs of the jobs.
+* const int* arrArrival: The time that the job 
+	arrived on the ready list.
+* const int* arrService: How long the job will 
+	take to complete.
+* const int jobNum: The length of the job list (arrays).
+*/
+void FirstCome(const int* arrPid, const int*  arrArrival, 
+	const int* arrService, const int jobNum)
+{
+	/*
+	Calculate:
+    1. The wait time for each process
+    2. The turnaround time for each process
+    3. The average wait time for all processes given to the simulation
+    4. The average turnaround time for all processes given to the simulation
+	*/
+
+	int curPid = 0;
+	int curArrivalTime = 0;
+	int curServiceTime = 0;
+	int curWaitTime = 0;
+	int curTurnaroundTime = 0;
+	int index = 0;
+
+	int currentTime = 0; // Keeps track of the time 
+	int totalWaitTime = 0; // Total wait time for all the jobs
+	int totalTurnaroundTime = 0; // Total turnaround time for all the jobs
+	int avgWaitTime = 0; // The average wait time
+	int avgTurnaroundTime = 0; // The average turnaround time
+
+	// Print which function is running
+	printf("%s\n", "First Come First Serve");
+
+	// Loop through each job, calculating everything
+	for(index = 0; index < jobNum; ++index)
+	{
+		// Wait time = how long process is waiting until first run.
+		// Turnaround time = time from process  first enters ready-state
+		// 	till he exits the running state for the last time.
+		curPid = arrPid[index];
+		curArrivalTime = arrArrival[index];
+		curServiceTime = arrService[index];
+
+		// Calculate the wait time
+		curWaitTime = abs(curArrivalTime - currentTime);
+		// Calculate the turnaround time
+		curTurnaroundTime = curWaitTime + curServiceTime;
+		// Increment our current time by how the the process ran (service time)
+		currentTime += curServiceTime; // Non-preemtive, service time = run time
+		totalWaitTime += curWaitTime;
+		totalTurnaroundTime += curTurnaroundTime;
+
+		// Print the PID, wait time, and turnaround time for the job
+		if(index == 0)
+			Print(curPid, curWaitTime, curTurnaroundTime, PRINT_HEADER);
+		else
+			Print(curPid, curWaitTime, curTurnaroundTime, PRINT_NORMAL);
+	}
+	// Print the Average wait time and turnaround time for all the jobs
+	avgWaitTime = totalWaitTime / jobNum;
+	avgTurnaroundTime = totalTurnaroundTime / jobNum;
+	Print(0, avgWaitTime, avgTurnaroundTime, PRINT_FOOTER);
+}
+
+/*
+** Print
+
+* Prints the job PID, how much time he waited to
+	start, and the turnaround time.
+
+* int header: Whether to print the header or not.
+* int pid: The PID of the job.
+* int waitTime: How long the job was in the read queue.
+* int turnaroundTime: Total time from first ready to done running.
+*/
+void Print(int pid, int waitTime, int turnaroundTime, int header)
+{
+	char *format = "%-15d %-15d %-15d \n"; // How to format the printing
+	char *headerFormat = "%-15s %-15s %-15s \n"; 
+	char *footerFormat = "\n%-20s %-20s \n";
+
+	if(header == PRINT_HEADER)
+		printf(headerFormat, "Process ID", "Wait Time", "Turnaround Time");
+	if(header == PRINT_FOOTER)
+	{
+		printf(footerFormat, "Average Wait Time", "Average Turnaround Time");
+		printf("%-20d %-20d \n", waitTime, turnaroundTime);
+		return; // Don't print the normal data
+	}	
+	printf(format, pid, waitTime, turnaroundTime);
 }
 
